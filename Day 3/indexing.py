6aab4af3c125383e560faf815e6633571def1d5d
Bishnu <thapabishnu20@gmail.com>
2023-04-17 13:48:48 +0545

    hands dirty with indexing methods

---
+++ Day 3/indexing.py
@@ -0,0 +1,11 @@
+text = "Hello World"
+
+# search from the left
+print(text.index("l"))
+
+# starts search from 6th index and ends to 10th index
+print(text.index("l", 9, 10))
+print(text[4])
+
+# search text from right to left
+print(text.rindex("l"))
