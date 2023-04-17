9d5c7fa2d1b239e5c4e129851defd219f1b8ea9a
Bishnu <thapabishnu20@gmail.com>
2023-04-17 12:41:27 +0545

    commission calculator created

---
+++ Day 2/commission.py
@@ -0,0 +1,4 @@
+name = input("Enter Your name: ")
+sales = int(input("Enter your Sales: "))
+commission = round((sales*13/100), 2)
+print(f"Hey {name}. Your commission for this month is: {commission}")
