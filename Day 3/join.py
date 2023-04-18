b7e1f14252682d169a26c844368b1de4e11c02c6
Bishnu <thapabishnu20@gmail.com>
2023-04-18 12:16:21 +0545

    find & replace string methods

--- Day 3/join.py
+++ Day 3/join.py
@@ -2,6 +2,14 @@ a = "Learning"
 b = "Python"
 c = "is"
 d = "Amazing"
-e = " ".join([a, b, c, d])
+e = " - ".join([a, b, c, d])
 
-print(e)
+# to find the index number of given character, if doesn't find the text returns -1 instead of error
+result = e.find("i")
+
+# replace text is with Bishnu
+
+newText = e.replace("is", "Bishnu")
+print(newText)
+
+# print(e)
