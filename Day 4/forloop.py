bd2ad2fd263ce5246dc1530c9dac22922ea15b3d
Bishnu <thapabishnu20@gmail.com>
2023-04-18 17:39:44 +0545

    forloop practice with examples

---
+++ Day 4/forloop.py
@@ -0,0 +1,4 @@
+my_list = ['a', 'b', 'c', 'd']
+
+for letters in my_list:
+    print(letters)
---
+++ Day 4/sumofnumbers.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5, 10]
+sum = 0
+
+for number in numbers:
+    sum = sum+number
+print(sum)
