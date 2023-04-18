e3124d2102635c9f92e8ec575afb4a4b1e6d5dab
Bishnu <thapabishnu20@gmail.com>
2023-04-18 12:58:07 +0545

    working with lists data type

---
+++ Day 3/lists.py
@@ -0,0 +1,16 @@
+my_list = ['a', 'b', 'c', 'z', 'x', 'y']
+# insert & delete in last element
+my_list.append("d")
+my_list.pop()
+
+# insert & delete in First element
+my_list.insert(0, "A")
+my_list.sort()
+my_list.reverse()
+
+print(my_list)
+print(type(my_list))
+print(len(my_list))
+
+print(my_list)
+print(my_list)
