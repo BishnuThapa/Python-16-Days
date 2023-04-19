f97d96ac342d54207ac5288508494bd97f068451
Bishnu <thapabishnu20@gmail.com>
2023-04-19 08:53:57 +0545

    implementation of enumerator, range and zip function

---
+++ Day 4/enumerator.py
@@ -0,0 +1,10 @@
+my_list = ['a', 'b', 'c', 'd']
+index = 0
+
+# for item in my_list:
+#     print(index, item)
+#     index += 1
+
+
+for index, item in enumerate(my_list):
+    print(index, item)
---
+++ Day 4/zip.py
@@ -0,0 +1,5 @@
+names = ['Bishnu', 'Yathartha', 'Khan']
+age = ['25', '27', '30']
+
+combination = list(zip(names, age))
+print(combination)
