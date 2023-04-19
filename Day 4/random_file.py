55d9765e2dd2f0b2c04b136ba02c3b66dbd53fe7
Bishnu <thapabishnu20@gmail.com>
2023-04-19 09:07:58 +0545

    shuffle example used in card game or multiple choices question answers

--- Day 4/random_file.py
+++ Day 4/random_file.py
@@ -3,6 +3,10 @@ from random import *
 # my_random = randint(1, 50)
 # print(my_random)
 
-colors = ['Red', 'Blue', 'Green', 'White']
-my_random = choice(colors)
-print(my_random)
+# colors = ['Red', 'Blue', 'Green', 'White']
+# my_random = choice(colors)
+# print(my_random)
+
+numbers = list(range(5, 50, 5))
+shuffle(numbers)
+print(numbers)
