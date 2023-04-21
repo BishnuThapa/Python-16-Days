cd9cc228986e5a0d00bb159321b1fb1524ff0960
Bishnu <thapabishnu20@gmail.com>
2023-04-21 20:11:21 +0545

    another example of return keyword

--- Day 5/multiply.py
+++ Day 5/multiply.py
@@ -1,6 +1,14 @@
+# def multiply(num1, num2):
+#     return num1 * num2
+
+
+# result = multiply(10, 10)
+# print(result)
+
+
 def multiply(num1, num2):
-    return num1 * num2
+    total = num1*num2
+    return total
 
 
-result = multiply(10, 10)
-print(result)
+print(multiply(5, 5))
