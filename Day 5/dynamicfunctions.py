265ff972c3bba9af5b46d57c8bee043c85d5dd09
Bishnu <thapabishnu20@gmail.com>
2023-04-22 20:16:03 +0545

    check in the list if the number is three digit number or not

--- Day 5/dynamicfunctions.py
+++ Day 5/dynamicfunctions.py
@@ -5,10 +5,26 @@
 # result = check_3_digits(45)
 # print(result)
 
-def check_3_digits(number):
-    return number in range(100, 1000)
+# def check_3_digits(number):
+#     return number in range(100, 1000)
 
 
-sum = 26+31
-result = check_3_digits(sum)
+# sum = 26+31
+# result = check_3_digits(sum)
+# print(result)
+
+
+# number of series to check
+
+def check_3_digits(list1):
+    three_digit_list = []
+    for n in list1:
+        if n in range(100, 1000):
+            three_digit_list.append(n)
+        else:
+            pass
+    return three_digit_list
+
+
+result = check_3_digits([555, 99, 6000])
 print(result)
