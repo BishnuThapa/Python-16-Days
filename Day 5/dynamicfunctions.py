0351e159ec4dff9c16e5932c17de48e10d3dd40d
Bishnu <thapabishnu20@gmail.com>
2023-04-22 20:05:34 +0545

    passing variable as a parameter

--- Day 5/dynamicfunctions.py
+++ Day 5/dynamicfunctions.py
@@ -1,6 +1,14 @@
+# def check_3_digits(number):
+#     return number in range(100, 1000)
+
+
+# result = check_3_digits(45)
+# print(result)
+
 def check_3_digits(number):
     return number in range(100, 1000)
 
 
-result = check_3_digits(45)
+sum = 26+31
+result = check_3_digits(sum)
 print(result)
