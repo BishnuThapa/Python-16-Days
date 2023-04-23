1e001437ee795d30aafcd6b01ca82d192288a639
Bishnu <thapabishnu20@gmail.com>
2023-04-23 21:42:05 +0545

    some manupulation with tuples in function

---
+++ Day 5/function_example.py
@@ -0,0 +1,11 @@
+coffee_prices = [('cappuccino', 1.5),
+                 ('espresso', 1.2),
+                 ('mocha', 1.9)
+
+                 ]
+
+# for element in coffee_prices:
+#     print(element)
+
+for coffee, price in coffee_prices:
+    print(coffee)
