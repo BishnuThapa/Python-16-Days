dd0e3f44adff1a9beccfaf33a6271442391c2ed7
Bishnu <thapabishnu20@gmail.com>
2023-04-23 21:50:55 +0545

    finding the most expensive coffe in the tuples

--- Day 5/function_example.py
+++ Day 5/function_example.py
@@ -10,5 +10,25 @@ coffee_prices = [('cappuccino', 1.5),
 # for coffee, price in coffee_prices:
 #     print(coffee)
 
-for coffee, price in coffee_prices:
-    print(price)
+# for coffee, price in coffee_prices:
+#     print(price)
+
+# to find most expensive coffee
+
+
+def most_expensive_coffee(list_of_prices):
+
+    highest_price = 0
+    my_most_expensive_coffee = ''
+
+    for coffee, price in list_of_prices:
+        if price > highest_price:
+            highest_price = price
+            my_most_expensive_coffee = coffee
+        else:
+            pass
+
+    return (my_most_expensive_coffee, highest_price)
+
+
+print(most_expensive_coffee(coffee_prices))
